a3dbeda707bdbb34df0a23d657d0ae3f0772e877
renatochitolina <renato.chitolina@db.tec.br>
2023-12-06 15:08:16 -0300

    Efetuando ajustes.

--- Capkcfjm_configuracao/build.gradle.kts
+++ Capkcfjm_configuracao/build.gradle.kts
@@ -6,9 +6,7 @@ subprojects {
 	apply(plugin = "bergs.pwx.gradle.testes.mobile")
 	
   dependencies {
-		implementation("org.seleniumhq.selenium:selenium-java:+")
-
-    // Use Rest Assured for REST API test automations
+		// Use Rest Assured for REST API test automations
     implementation("io.rest-assured:rest-assured:5.3.0")
 
     // Use JSON Path for extra API JSON response handling
