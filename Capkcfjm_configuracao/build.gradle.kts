45dca55501f5547a60eedc5c9f16b14285b174b2
renatochitolina <renato.chitolina@db.tec.br>
2023-12-10 15:24:17 -0300

    Atualizando definições do arquivo de build do Gradle. Adicionando tarefa de execução da aplicação main via Gradle. Atualizando configurações de logging de testes via linha de comando.

--- Capkcfjm_configuracao/build.gradle.kts
+++ Capkcfjm_configuracao/build.gradle.kts
@@ -1,15 +1,37 @@
 plugins {
+    // Using "Banrisul mobile framework" plugin to leverage its capabilities
     id("bergs.pwx.gradle.testes.mobile")
+
+    // Using "application" plugin to make it possible to run the "main" project piece
+    id("application")
 }
 
 subprojects {
-	apply(plugin = "bergs.pwx.gradle.testes.mobile")
+	// Applying the "Banrisul mobile framework" on subprojects also
+  apply(plugin = "bergs.pwx.gradle.testes.mobile")
 	
   dependencies {
-		// Use Rest Assured for REST API test automations
+		// Using Rest Assured for REST API test automations
     implementation("io.rest-assured:rest-assured:5.3.0")
 
-    // Use JSON Path for extra API JSON response handling
+    // Using JSON Path for extra API JSON response handling
     implementation("io.rest-assured:json-path:5.3.0")
 	}
+
+  application {
+    // Setting up the application entry point
+    mainClassName = "app.WebServer"
+  }
+
+  tasks.withType(Test) {
+      // Ensuring that "test" tasks utilise the required setup for JUnit 5
+      useJUnitPlatform()
+
+      // Setting up logging information for "test" tasks
+      testLogging {
+          exceptionFormat("full")
+          events("started", "skipped", "passed", "failed")
+          showStandardStreams = false
+      }
+  }
 }
--- Capkcfjm_configuracao/settings.gradle.kts
+++ Capkcfjm_configuracao/settings.gradle.kts
@@ -1,5 +1,6 @@
 buildscript {
     repositories.maven("https://repobins.apps.prod.banrisul.com.br/repository/maven-public/")
+    
     dependencies.classpath("bergs.pwx.build.plugins:mmd-gradle-plugin:${extra["mm.plugin.version"]}")
 }
 apply(plugin = "bergs.pwx.gradle.settings")
